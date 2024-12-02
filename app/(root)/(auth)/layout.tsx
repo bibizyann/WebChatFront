3392845261887514d1f95d0e5e0d20b1bcf8e003
Vladimir Sclyarov <sklyarovvl@mail.ru>
2024-12-02 17:50:43 +0300

    Fix: theme matching

--- app/(root)/(auth)/layout.tsx
+++ app/(root)/(auth)/layout.tsx
@@ -1,33 +1,8 @@
 'use client';
 
-import React, {ReactNode, useEffect} from 'react';
-import "@/app/globals.css"
+import React, {ReactNode} from 'react';
 
 const AuthLayout = ({children}: {children: ReactNode}) => {
-    useEffect(() => {
-        const savedTheme = localStorage.getItem("theme");
-        if (savedTheme === 'dark') {
-            document.documentElement.classList.add('dark');
-        } else if (savedTheme === 'light') {
-            document.documentElement.classList.remove('dark');
-        }
-
-        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-        const handleSystemThemeChange = () => {
-            if (!savedTheme || savedTheme === "system") {
-                if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                    document.documentElement.classList.add('dark');
-                } else {
-                    document.documentElement.classList.remove('dark');
-                }
-            }
-        };
-
-        mediaQuery.addEventListener("change", handleSystemThemeChange);
-        return () => {
-            mediaQuery.removeEventListener("change", handleSystemThemeChange);
-        };
-    }, []);
     return (
         <div className="relative w-full">
             {children}
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,40 @@
-import React, {ReactNode} from 'react'
+'use client';
+
+import React, {ReactNode, useEffect} from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else if (savedTheme === 'light') {
+            document.documentElement.classList.remove('dark');
+        } else {
+            const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            if (systemPrefersDark) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+        }
+
+        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+        const handleSystemThemeChange = () => {
+            if (!savedTheme || savedTheme === "system") {
+                if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    document.documentElement.classList.add('dark');
+                } else {
+                    document.documentElement.classList.remove('dark');
+                }
+            }
+        };
+
+        mediaQuery.addEventListener("change", handleSystemThemeChange);
+        return () => {
+            mediaQuery.removeEventListener("change", handleSystemThemeChange);
+        };
+    }, []);
+
     return(
         <main>
             {children}
