3c43cc118e4d3acb34a391acbc1574af95592d4c
Vladimir Sclyarov <sklyarovvl@mail.ru>
2024-12-11 20:51:15 +0300

    Try to fix Cookie Houdini cosplay

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,20 @@ const publicRoutes = ['/login', '/registration']
 export default async function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname
     const isPublicRoute = publicRoutes.includes(path)
+    const { headers } = await fetch(req);
 
     const cookie = cookies().get('Authorization')?.value
     const session = await decrypt(cookie)
+
+    if (headers.has("Set-Cookie")) {
+        const cookies = headers.get("Set-Cookie")!;
+
+        // Replace the Domain attribute to localhost
+        const modifiedCookies = cookies.replace(/Domain=[^;]+/, "Domain=webchatfront-6xch.vercel.app");
+
+        // Set the modified Set-Cookie header
+        headers.set("Set-Cookie", modifiedCookies);
+    }
 
     if (
         isPublicRoute &&
@@ -19,5 +30,7 @@ export default async function middleware(req: NextRequest) {
         return NextResponse.redirect(new URL('/', req.nextUrl))
     }
 
-    return NextResponse.next()
+    return NextResponse.next({
+        headers: headers,
+    })
 }
