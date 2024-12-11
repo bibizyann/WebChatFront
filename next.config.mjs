1bd26520baf5a5e1323dc18077424188e08bbacf
Vladimir Sclyarov <sklyarovvl@mail.ru>
2024-12-11 21:25:42 +0300

    Try to fix Cookie Houdini cosplay /2

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-// module.exports = {
-//     experimental: {
-//         serverActions: {
-//             allowedOrigins: ['???', '???'],
-//         },
-//     },
-// }
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "webchatmirea.onrender.com" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Set-Cookie, Date, X-Api-Version" },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin'
+                    }
+                ]
+            }
+        ]
+    }
+};
 
 export default nextConfig;
